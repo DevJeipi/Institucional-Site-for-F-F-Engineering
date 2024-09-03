63e24f101e7420ff9054843fa401f2088e5c56ad
DevJeipi <joaopedroghilardi@gmail.com>
2024-09-03 16:49:29 -0300

    x

--- compliance.php
+++ compliance.php
@@ -7,22 +7,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./imagens/favicon.ico" />
     <link rel="stylesheet" href="./css/stylecompliance.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap">
 </head>
 <title>Canal de Denúncia | F&F Engenharia</title>
 
 <body>
     <main>
-        <div class="logo"><img src="../imagens/logotipo_f_e_f-removebg-preview.png" alt="Logo F&F Engenharia"></div>
-        <h1></h1>
+        <div class="logo"><img src="./imagens/logotipo_f_e_f-removebg-preview.png" alt="Logo F&F Engenharia"></div>
+        <h1>Canal De Denúncia</h1> 
         <form action="denuncia.php" method="post">
             <label for="nome">Nome(Opcional):</label>
-            <input type="text" id="nome" name="nome">
+            <input class="form-input" type="text" id="nome" name="nome">
 
             <label for="email">E-mail(Opcional):</label>
-            <input type="email" id="email" name="email">
+            <input class="form-input" type="email" id="email" name="email">
 
             <label for="motivo">Motivo:</label>
-            <select name="motivo" id="motivo" required>
+            <select class="form-input" name="motivo" id="motivo" aria-placeholder="Sim" required>
+                <option value="escolher">--Escolha uma opção--</option>
                 <option value="Sugestão">Sugestão</option>
                 <option value="Crítica">Crítica</option>
                 <option value="Dúvida">Dúvida</option>
@@ -30,9 +34,9 @@
             </select>
 
             <label for="mensagem">Mensagem:</label>
-            <textarea name="mensagem" id="mensagem" required></textarea>
+            <textarea class="form-input" name="mensagem" id="mensagem" required></textarea>
 
-            <input type="button" value="botão" class="btn-form">
+            <input class="form-input btn" type="button" value="Enviar" class="btn-form">
         </form>
     </main>
 </body>
