8dc90b3c44aa3db4d293f469492a10be22d997d6
DevJeipi <joaopedroghilardi@gmail.com>
2024-09-04 20:37:40 -0300

    t

--- denuncia.php
+++ denuncia.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = isset($_POST['nome']) ? $_POST['nome'] : 'Anônimo';
-    $email = isset($_POST['email']) ? $_POST['email'] : 'joao.hubner07@gmail.com';
+    $email = isset($_POST['email']) ? $_POST['email'] : 'joaopedroghilardi@gmail.com';
     $motivo = $_POST['motivo'];
     $mensagem = $_POST['mensagem'];
 
-    $to = "joaopedroghilardi@gmail.com";
+    $to = "joao.hubner07@gmail.com";
     $subject = "Nova Denúncia: $motivo";
     $body = "Nome: $nome\nEmail: $email\nMotivo: $motivo\n\nMensagem:\n$mensagem";
     $headers = "From: $email";
